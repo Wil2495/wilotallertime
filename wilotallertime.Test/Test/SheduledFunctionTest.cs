698d3c1e8bb1d6307e8485af5ad890a95f531653
wil2495 <alexisfranco2495@hotmail.com>
2021-09-05 10:50:03 -0500

    Add SheduledFuction

---
+++ wilotallertime.Test/Test/SheduledFunctionTest.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using wilotallertime.Functions.Functions;
+using wilotallertime.Test.Helpers;
+using Xunit;
+
+namespace wilotallertime.Test.Tests
+{
+    public class SheduledFunctionTest
+    {
+
+
+        [Fact]
+        public void ScheduledFuction_Should_Log_Message()
+        {
+            // Arrange
+            MockCloudTableTimes mockTimes = new MockCloudTableTimes(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            MockCloudTableConsolidated mockConsolidated = new MockCloudTableConsolidated(new Uri("http://127.0.0.1:10002/devstoreaccount1/reports"));
+            ListLogger logger = (ListLogger)TestFactoryConsolidated.CreateLogger(LoggerTypes.List);
+
+            // Add
+            ScheduledFunction.Run(null, mockTimes, mockConsolidated, logger);
+            string message = logger.Logs[0];
+
+            // Assert
+            Assert.Contains($"New consolidation process, starts at: {DateTime.Now}", message);
+        }
+    }
+}
+
