f736a553b5d0294c8c1ac9873cf8da4cbb38434c
wil2495 <alexisfranco2495@hotmail.com>
2021-09-04 22:16:04 -0500

    Add Models folder

---
+++ wilotallertime.Common/Models/Consolidated.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace wilotallertime.Common.Models
+{
+    internal class Consolidated
+    {
+        public int IdEmployee { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int MinutesWork { get; set; }
+    }
+}
---
+++ wilotallertime.Common/Models/Time.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace wilotallertime.Common.Models
+{
+    internal class Time
+    {
+        public int? IdEmployee { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public int? Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
