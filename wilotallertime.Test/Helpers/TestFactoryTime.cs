a38d919ac20ed35c8111bdacdfd40be3f0e11332
wil2495 <alexisfranco2495@hotmail.com>
2021-09-04 23:18:07 -0500

    add MockCloudTableTimes and TestFactory

---
+++ wilotallertime.Test/Helpers/TestFactoryTime.cs
@@ -0,0 +1,103 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using wilotallertime.Common.Models;
+using wilotallertime.Functions.Entities;
+
+namespace wilotallertime.Test.Helpers
+{
+    public class TestFactoryTime
+    {
+
+
+        public static TimeEntity GetTimeEntity()
+        {
+            return new TimeEntity
+            {
+                Consolidated = false,
+                Date = DateTime.UtcNow,
+                IdEmployee = 78,
+                ETag = "*",
+                PartitionKey = "TIME",
+                RowKey = Guid.NewGuid().ToString(),
+                Type = 0
+            };
+        }
+
+
+        public static DefaultHttpRequest CreateHttpRequest(Guid id, Time timeRequest)
+        {
+            string request = JsonConvert.SerializeObject(timeRequest);
+            return new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Body = GenerateStreamFromString(request),
+                Path = $"/{id}"
+            };
+        }
+
+        public static DefaultHttpRequest CreateHttpRequest(Guid id)
+        {
+            return new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Path = $"/{id}"
+            };
+        }
+
+        public static DefaultHttpRequest CreateHttpRequest(Time timeRequest)
+        {
+            string request = JsonConvert.SerializeObject(timeRequest);
+            return new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Body = GenerateStreamFromString(request)
+            };
+        }
+
+        public static DefaultHttpRequest CreateHttpRequest()
+        {
+            return new DefaultHttpRequest(new DefaultHttpContext());
+        }
+
+
+        public static Time GetTimeRequest()
+        {
+            return new Time
+            {
+                Consolidated = false,
+                Date = DateTime.UtcNow,
+                IdEmployee = 5,
+                Type = 5
+            };
+        }
+
+
+        public static Stream GenerateStreamFromString(string stringToConvert)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(stringToConvert);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        public static ILogger CreateLogger(LoggerTypes type = LoggerTypes.Null)
+        {
+            ILogger logger;
+            if (type == LoggerTypes.List)
+            {
+                logger = new ListLogger();
+            }
+            else
+            {
+                logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+            }
+
+            return logger;
+        }
+    }
+}
